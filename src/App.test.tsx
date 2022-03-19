a43b5f7f98376b57d5dd06d948b4fd9fe07aa917
Fabio Salimbeni <fabio.salimbeni@gmail.com>
2022-03-19 00:34:09 +0000

    Add name, email and rating fields

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import App from "./App"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("Customer Feedback Page", () => {
+  it("should render the Name field", () => {
+    const { getByLabelText } = render(<App />)
+    expect(getByLabelText("Name:")).toBeInTheDocument()
+  })
+
+  it("should render the Email field", () => {
+    const { getByLabelText } = render(<App />)
+    expect(getByLabelText("Email:")).toBeInTheDocument()
+  })
+
+  it("should render the Rating field", () => {
+    const { getByLabelText } = render(<App />)
+    expect(getByLabelText("Rating:")).toBeInTheDocument()
+  })
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import styled from "styled-components"
 
 function App() {
+  const [name, setName] = useState<string>("")
+  const [email, setEmail] = useState<string>("")
+  const [rating, setRating] = useState<number>(0)
+
+  const handleRatingValue: (e: React.ChangeEvent<HTMLInputElement>) => void = (
+    e
+  ) => {
+    setRating(parseInt(e.target.value))
+  }
+
+  // useEffect(() => {
+  //   console.log(name, email, rating)
+  // })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header" />
+      <main>
+        <div>
+          <Label htmlFor="name">Name:</Label>
+          <Input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Name"
+          />
+        </div>
+        <div>
+          <Label htmlFor="email">Email:</Label>
+          <Input
+            type="text"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+        </div>
+        <div>
+          <Label htmlFor="rating">Rating:</Label>
+          <Input
+            type="radio"
+            id="rating"
+            name="1"
+            value="1"
+            checked={rating === 1}
+            onChange={handleRatingValue}
+          />{" "}
+          1
+          <Input
+            type="radio"
+            id="rating"
+            name="2"
+            value="2"
+            checked={rating === 2}
+            onChange={handleRatingValue}
+          />{" "}
+          2
+          <Input
+            type="radio"
+            id="rating"
+            name="3"
+            value="3"
+            checked={rating === 3}
+            onChange={handleRatingValue}
+          />{" "}
+          3
+          <Input
+            type="radio"
+            id="rating"
+            name="4"
+            value="4"
+            checked={rating === 4}
+            onChange={handleRatingValue}
+          />{" "}
+          4
+          <Input
+            type="radio"
+            id="rating"
+            name="5"
+            value="5"
+            checked={rating === 5}
+            onChange={handleRatingValue}
+          />{" "}
+          5
+        </div>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+const Label = styled.label`
+  display: none;
+`
+
+const Input = styled.input`
+  border-radius: 5px;
+  border: 1px solid black;
+  padding: 5px;
+  line-height: 16px;
+  font-size: 12px;
+`
+
+export default App
